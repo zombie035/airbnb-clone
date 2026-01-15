5a577f0e8af38e16fbb7f3ada859057c1dd73e04
zombie035 <167063563+zombie035@users.noreply.github.com>
2026-01-16 01:15:24 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,19 +1,29 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
   firstName: {
     type: String,
-    required: [true, 'First name is required']
+    required: [true, 'First name is required'],
+    trim: true,
+    minlength: [2, 'First name must be at least 2 characters']
   },
-  lastName: String,
+  lastName: {
+    type: String,
+    trim: true
+  },
   email: {
     type: String,
     required: [true, 'Email is required'],
-    unique: true
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email']
   },
   password: {
     type: String,
-    required: [true, 'Password is required']
+    required: [true, 'Password is required'],
+    minlength: [8, 'Password must be at least 8 characters']
   },
   userType: {
     type: String,
@@ -24,7 +34,6 @@ const userSchema = mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Home'
   }],
-  
   bookings: [{
     home: {
       type: mongoose.Schema.Types.ObjectId,
@@ -36,5 +45,23 @@ const userSchema = mongoose.Schema({
     }
   }]
 });
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Method to compare password
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
